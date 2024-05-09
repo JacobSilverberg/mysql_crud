a3fb9a7342302afb1e0775ddc659d8f2348a01ca
Jacob Silverberg <minkus962@gmail.com>
2024-05-09 18:49:05 -0400

    basic backend setup

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express"
+import mysql from "mysql2"
+
+const app = express()
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "1337",
+    database: "test"
+})
+
+app.get("/", (req, res) => {
+    res.json("hello this is the backend")
+})
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books"
+    db.query(q, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+    const values = ["title2", "desc2", "cover.backend"]
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    }
+        
+    )
+})
+
+app.listen(8800, () => {
+    console.log("Connected to backend!")
+})
