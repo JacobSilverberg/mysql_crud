c3a907c1e773773a406eb848f6766562e406bc6f
Jacob Silverberg <minkus962@gmail.com>
2024-05-12 21:24:23 -0400

    Read functional. 27:41

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import mysql from "mysql2"
+import cors from "cors"
 
 const app = express()
 
@@ -10,6 +11,9 @@ const db = mysql.createConnection({
     password: "1337",
     database: "test"
 })
+
+app.use(express.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.json("hello this is the backend")
@@ -25,11 +29,15 @@ app.get("/books", (req, res) => {
 
 app.post("/books", (req, res) => {
     const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
-    const values = ["title2", "desc2", "cover.backend"]
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover
+    ]
 
     db.query(q, [values], (err, data) => {
         if (err) return res.json(err)
-        return res.json(data)
+        return res.json("Book has been created.")
     }
         
     )
